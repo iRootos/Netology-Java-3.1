aabb7d7a512f7b6c1a9abd6f387538b24f667e98
Rootos <rootos@hotmail.com>
2022-04-26 17:02:02 +0300

    initial project

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 125;
+        int bonus = ticketPrice / 20;
+        System.out.println("Бонусные мили: " + bonus);
+    }
+}
